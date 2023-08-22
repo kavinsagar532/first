c39992093fe0e9c9842b72a006c710ee3ec54967
kavinsagar532 <kavinsagar532@gmail.com>
2023-08-22 13:18:13 +0530

    first commit

---
+++ amazon/orders/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+# Register your models here.
+from orders.models import data1
+admin.site.register(data1)
---
+++ amazon/shipping/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+# Register your models here.
+from shipping.models import data2
+admin.site.register(data2)
